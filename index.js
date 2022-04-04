b0278e650ac2af1cd654a55dbaacb0e6ebf326dd
wteo <wendyteo.wy@gmail.com>
2022-04-05 07:09:28 +1000

    Added more shapes and colours to shapes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter; // All this are accessed from the Matter JS script link found in index.html
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800, // all in px value
-        height: 600,        
+        wireframes: false,// this gives solid shapes. Random colours will be given to the shapes
+        width, 
+        height        
     }
 }); // this create a canvas element in html
 Render.run(render);
@@ -27,12 +31,37 @@ World.add(world, shape); // this calls the shape to show up in the screen
 
 //Walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true}),
+    Bodies.rectangle(400, // horizontal position 
+        0, // vertical position
+        800, // width
+        40, // height
+        { isStatic: true}),
     Bodies.rectangle(400, 600, 800, 40, { isStatic: true}),
     Bodies.rectangle(0, 300, 40, 600, { isStatic: true}),
     Bodies.rectangle(800, 300, 40, 600, { isStatic: true})
 ];
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random Shapes
+for (let i = 0; i < 20; i++) {
+    if (Math.random() > 0.5) {
+        World.add(world, Bodies.rectangle(
+            Math.random() * width, // randomized these positions so shapes will pop in any random places in the screen.
+            Math.random() * height, // Otherwise, they will stacked against each other.
+            50, 50));
+    } else {
+        World.add(
+            world,
+            Bodies.circle(
+                Math.random() * width, 
+                Math.random() * height,
+                35, // radius
+                {
+                    render: {
+                        fillStyle: "red"
+                    }
+                })
+        )
+    }
+} // The loop should determine the number of shapes in screen
 
