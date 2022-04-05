197fc82b114af7b1a143415e11d44895e5c1082e
wteo <wendyteo.wy@gmail.com>
2022-04-05 11:11:10 +1000

    Created horizontal and vertical walls in arrays

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter; // All this are accessed from the Matter JS script link found in index.html
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -29,10 +30,20 @@ World.add(world, walls);
 // Maze generation
 // false - starts off as false to show cell in question hasn't been touched yet
 
-const grid = Array(3)
-.fill(null)
-.map(() => Array(3).fill(false));
-// map method is used insteasd of adding [false, false, false] to fill.
-// map method preferred as this allows dev to manipulate each row of the array individually.
-// We can't do this with the latter.
-console.log(grid);
+const grid = Array(cells)
+    .fill([])
+    .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+    .fill([])
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill([])
+    .map(() => Array(cells).fill(false));
+
+// Code below the picked starting cell is randomized
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells)
+
+console.log(startRow, startColumn);
