f60d8e29d46a7a6b97d55dcf8da47917e9734ad9
wteo <wendyteo.wy@gmail.com>
2022-04-08 05:49:03 +1000

    Added eventListener to starting ball

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies } = Matter; // All this are accessed from the Matter JS script link found in index.html
+const { Engine, Render, Runner, World, Bodies, Body } = Matter; // All this are accessed from the Matter JS script link found in index.html
 
 const cells = 3;
 const width = 600;
@@ -168,3 +168,20 @@ const ball = Bodies.circle(
     unitLength / 4
 );
 World.add(world, ball);
+
+document.addEventListener("keydown", event => {
+    const { x, y } = ball.velocity;
+
+    if (event.keyCode === 87) {
+        Body.setVelocity(ball, { x, y: y - 5 });
+    }
+    if (event.keyCode === 68) {
+        Body.setVelocity(ball, { x: x + 5, y });
+    }
+    if (event.keyCode === 83) {
+        Body.setVelocity(ball, { x, y: y + 5 });
+    }
+    if (event.keyCode === 65) {
+        Body.setVelocity(ball, { x: x - 5, y });
+    }
+});
