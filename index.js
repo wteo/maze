86f31ed19d33914add166882a6077f1c3591798a
wteo <wendyteo.wy@gmail.com>
2022-04-08 05:30:45 +1000

    Added starting ball

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ Runner.run(Runner.create(), engine);
 
 //Walls
 const walls = [
-    Bodies.rectangle(width/2, 0, width, 40, { isStatic: true}),
-    Bodies.rectangle(width/2, height, width, 40, { isStatic: true}),
-    Bodies.rectangle(0, height/2, 40, height, { isStatic: true}),
-    Bodies.rectangle(width, width/2, 40, height, { isStatic: true})
+    Bodies.rectangle(width/2, 0, width, 2, { isStatic: true}),
+    Bodies.rectangle(width/2, height, width, 2, { isStatic: true}),
+    Bodies.rectangle(0, height/2, 2, height, { isStatic: true}),
+    Bodies.rectangle(width, width/2, 2, height, { isStatic: true})
 ];
 World.add(world, walls);
 
@@ -147,6 +147,8 @@ verticals.forEach((row,rowIndex) => {
     });
 });
 
+// Goal
+
 const goal = Bodies.rectangle(
     width - unitLength / 2,
     height - unitLength / 2,
@@ -157,3 +159,12 @@ const goal = Bodies.rectangle(
     }
 );
 World.add(world, goal);
+
+// Ball
+
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 4
+);
+World.add(world, ball);
